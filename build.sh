36f732d8bc988e75513d607e977b1eba4733d5f3
Mattia Basone <mattia.basone@gmail.com>
2022-10-31 20:36:37 +0100

    fix cron and update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for PHP_VERSION in 5.6 7.0 7.1 7.2 7.4 8.0 8.1
+for PHP_VERSION in 7.2 7.3 7.4 8.0 8.1
 do
 	docker build . -f Dockerfile \
   --build-arg PHP_VERSION=${PHP_VERSION} \
